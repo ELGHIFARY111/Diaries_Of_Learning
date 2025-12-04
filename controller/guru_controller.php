28a743e4bd92d5b909307c791dc600c13e36d296
ELGHIFARY111 <elgifaryamink@gmail.com>
2025-12-04 08:44:02 +0700

    nav

--- controller/guru_controller.php
+++ controller/guru_controller.php
@@ -3,25 +3,32 @@ require_once "./model/role.php";
 
 
 function navigasi_guru() {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/navigasiGuru.php";
 }
 
 function guru_dashboard_page($koneksi) {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/dashboard_guru.php";
 }
 function laporan_progres_page($koneksi) {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/laporan_progres.php";
 }
 function misi_kosakata_page($koneksi) {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/misi_kosakata.php";
 }
 function monitoring_page($koneksi) {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/monitoring.php";
 }
 function profil_page($koneksi) {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/profil.php";
 }
 function review_catatan_page($koneksi) {
+    $active = $_GET['active'] ?? 'aktif';
     include "./views/guru/review_catatan.php";
 }
 
--- views/guru/navigasiGuru.php
+++ views/guru/navigasiGuru.php
@@ -1,32 +1,33 @@
+<link rel="stylesheet" href="./views/css/novigasi_guru.css">
 <div class="sidebar">
       <div class="logo">📘 Diary of Learning</div>
       <div class="menu">
-         <a href="index.php?page=guru" 
+         <a href="index.php?page=guru&active=dashboard&aktif=true" 
             class="<?= ($active == 'dashboard') ? 'aktif' : '' ?>">
             🏠 Dashboard
          </a>
 
-         <a href="index.php?page=guru/monitoring" 
+         <a href="index.php?page=guru/monitoring&active=siswa&aktif=true" 
             class="<?= ($active == 'siswa') ? 'aktif' : '' ?>">
             🧑‍🎓 Daftar Siswa
          </a>
 
-         <a href="index.php?page=guru/misi_kosakata" 
+         <a href="index.php?page=guru/misi_kosakata&active=misi&aktif=true" 
             class="<?= ($active == 'misi') ? 'aktif' : '' ?>">
             🎯 Misi Kosa Kata Sekolah
          </a>
 
-         <a href="index.php?page=guru/review_catatan" 
+         <a href="index.php?page=guru/review_catatan&active=review&aktif=true" 
             class="<?= ($active == 'review') ? 'aktif' : '' ?>">
             📝 Review Catatan Siswa
          </a>
 
-         <a href="index.php?page=guru/laporan_progres" 
+         <a href="index.php?page=guru/laporan_progres&active=laporan&aktif=true" 
             class="<?= ($active == 'laporan') ? 'aktif' : '' ?>">
             📊 Laporan Progres
          </a>
 
-         <a href="index.php?page=guru/profil" 
+         <a href="index.php?page=guru/profil&active=profil&aktif=true" 
             class="<?= ($active == 'profil') ? 'aktif' : '' ?>">
             👤 Profil Guru
          </a>
